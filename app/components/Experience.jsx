28c7fa66b5e9c00c1441b1fe0d13f385cfaff915
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-10-15 14:10:28 +0100

    Animating the cube rotation

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -1,11 +1,21 @@
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+
 export default function Experience() {
+
+  const cubeRef = useRef()
+
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta
+  })
+
   return (
     <>
       <mesh position-x={ -2 }>
         <sphereGeometry />
         <meshBasicMaterial color="orange" />
       </mesh>
-      <mesh rotation-y={Math.PI * .25} position-x={2} scale={1.5}>
+      <mesh ref={ cubeRef } rotation-y={Math.PI * .25} position-x={2} scale={1.5}>
         <boxGeometry scale={1.5} />
         <meshBasicMaterial color="mediumpurple" />
       </mesh>

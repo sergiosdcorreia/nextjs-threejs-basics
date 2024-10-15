da8d22c3de1ae3daf870f80d7b2d0ccf3bdb25e4
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-10-15 13:55:46 +0100

    Adding an orange sphere, and a purple box.

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -1,8 +1,12 @@
 export default function Experience() {
   return (
     <>
-      <mesh>
-        <sphereGeometry args={[ 1.5, 32, 32 ]} />
+      <mesh position-x={ -2 }>
+        <sphereGeometry />
+        <meshBasicMaterial color="orange" />
+      </mesh>
+      <mesh rotation-y={Math.PI * .25} position-x={2}>
+        <boxGeometry scale={1.5} />
         <meshBasicMaterial color="mediumpurple" wireframe />
       </mesh>
     </>

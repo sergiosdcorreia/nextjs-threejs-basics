4036948fdd9314297d7068cd2ec5992e20f9e217
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-10-15 14:28:17 +0100

    Adding lights to the scene.

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -4,24 +4,30 @@ import { useRef } from "react"
 export default function Experience() {
 
   const cubeRef = useRef()
+  const groupRef = useRef()
 
   useFrame((state, delta) => {
     cubeRef.current.rotation.y += delta
+    groupRef.current.rotation.y += delta
   })
 
   return (
     <>
-      <mesh position-x={ -2 }>
-        <sphereGeometry />
-        <meshBasicMaterial color="orange" />
-      </mesh>
-      <mesh ref={ cubeRef } rotation-y={Math.PI * .25} position-x={2} scale={1.5}>
-        <boxGeometry scale={1.5} />
-        <meshBasicMaterial color="mediumpurple" />
-      </mesh>
+      <directionalLight position={ [ 1, 2, 3 ]} intensity={ 4.5 } />
+      <ambientLight intensity={ 1.5 } />
+      <group ref={ groupRef }>
+        <mesh position-x={ -2 }>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+        <mesh ref={ cubeRef } rotation-y={Math.PI * .25} position-x={2} scale={1.5}>
+          <boxGeometry scale={1.5} />
+          <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+      </group>
       <mesh position-y={-1} rotation-x={ -Math.PI * .5} scale={10}>
         <planeGeometry />
-        <meshBasicMaterial color="greenyellow" />
+        <meshStandardMaterial color="greenyellow" />
       </mesh>
     </>
   );
